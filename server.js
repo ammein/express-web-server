ff5b259a04262db5efbbc0b7161752ad0a4e6499
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-08 23:53:34 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 // Initialize express
 var app = express();
 // dynamic templates
@@ -79,6 +80,6 @@ app.get('/bad' , (req , res) => {
         errorMessage : 'Unable to handle request'
     });
 });
-app.listen(3000 , () => {
-    console.log("Server is up on port 3000");
+app.listen(port , () => {
+    console.log(`Server is up on port ${port}`);
 });
